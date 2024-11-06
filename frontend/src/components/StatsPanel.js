36edc4a3ffbb3c015463ff3dddf8a82366c6a052
Zayd Hassan <zayd_hassan@polysia.com>
2024-11-06 12:05:23 +0530

    Updated StatsPanel.js

--- frontend/src/components/StatsPanel.js
+++ frontend/src/components/StatsPanel.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import StatBox from './StatBox';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'; 
+import { ThemeContext } from './ThemeContext'; 
 
 const PanelContainer = styled.div`
   padding: 14px;
   height: 100vh;
-  width: 245px;
+  width: 247px;
   overflow-y: auto;
-  background: #000;
+  background: ${({ theme }) => theme === 'dark' ? '#000' : '#fff'};
   position: fixed;
   right: ${props => (props.isOpen ? '0' : '-300px')}; 
   top: 0;
@@ -34,47 +35,48 @@ const ToggleIcon = styled.div`
 `;
 
 const QuoteBox = styled.div`
-  color: white;
-  background-color: #1A2S5E;
+  color: ${({ theme }) => theme === 'dark' ? 'white' : 'black'};
+  background-color: ${({ theme }) => theme === 'dark' ? '#000' : '#fff'};
   padding: 10px;
   margin-bottom: 20px;
   text-align: center;
   border: 1.9px solid #3478f6;
-  font-size: 0.9em;
+  font-size: 0.92em;
   font-weight: bold;
-  margin-top: 59px;
+  margin-top: 51px;
   z-index: 999;
 `;
 
 const StatsPanel = ({ isSidebarOpen, toggleSidebar }) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <>
-      <PanelContainer isOpen={isSidebarOpen}>
-        <QuoteBox>SAVE YOURSELF FROM CYBER ATTACK</QuoteBox>
+      <PanelContainer isOpen={isSidebarOpen} theme={theme}>
+        <QuoteBox theme={theme}>SAVE YOURSELF FROM CYBER ATTACK</QuoteBox>
         <StatBox
-          title="Types of Cyber Attacks"
+          title="TYPES OF CYBER ATTACKS"
           items={[
-            { name: 'Phishing', color: 'red' },
-            { name: 'DDoS', color: 'yellow' },
-            { name: 'Malware', color: 'orange' }
+            { name: 'PHISHING', color: 'red' },
+            { name: 'DDOS', color: 'yellow' },
+            { name: 'MALWARE', color: 'orange' }
           ]}
         />
         <StatBox
-          title="Targeted Nations"
+          title="TARGETED NATIONS"
           items={[
-            { name: 'Ethiopia', flag: '/ethiopia.png' },
-            { name: 'Mongolia', flag: '/mongolia.png' },
-            { name: 'Nepal', flag: '/nepal.webp' },
-            { name: 'Angola', flag: '/angola.png' }
+            { name: 'ETHIOPIA', flag: '/ethiopia.png' },
+            { name: 'MONGOLIA', flag: '/mongolia.png' },
+            { name: 'NEPAL', flag: '/nepal.webp' },
+            { name: 'ANGOLA', flag: '/angola.png' }
           ]}
         />
         <StatBox
-          title="Top Targeted Industries"
+          title="TARGETED INDUSTRIES"
           items={[
-            { name: 'Education', flag: '/education.png' },
-            { name: 'Government', flag: '/govt.png' },
-            { name: 'Telecommunication', flag: '/mobile.png' },
-            { name: 'Healthcare', flag: '/healthcare.png' }
+            { name: 'EDUCATION', flag: '/education.png' },
+            { name: 'GOVERNMENT', flag: '/govt.png' },
+            { name: 'TELECOMMUNICATION', flag: '/mobile.png' },
+            { name: 'HEALTHCARE', flag: '/healthcare.png' }
           ]}
         />
       </PanelContainer>
@@ -84,5 +86,4 @@ const StatsPanel = ({ isSidebarOpen, toggleSidebar }) => {
     </>
   );
 };
-
 export default StatsPanel;
