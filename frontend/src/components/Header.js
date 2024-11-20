6d5a97cb79fdd08f681b00c82c3cfd3213b5555d
Zayd Hassan <zayd_hassan@polysia.com>
2024-11-20 12:33:15 +0530

    Updated Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
-import { MdLightMode, MdDarkMode } from 'react-icons/md'; 
+import { MdLightMode, MdDarkMode, MdVisibility } from 'react-icons/md'; 
 
-const Header = () => {
+const Header = ({ onToggleIncidents }) => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <div className="header">
       <img src="/Polysia.png" alt="Logo" className="logo" />
       <span className="title">THREATMAP LIVE ATTACK</span>
+      <button onClick={onToggleIncidents} className="incident-toggle">
+        <MdVisibility size={28} /> 
+      </button>
       <button onClick={toggleTheme} className="theme-toggle">
         {theme === 'dark' ? <MdLightMode size={28} /> : <MdDarkMode size={28} />}
       </button>
