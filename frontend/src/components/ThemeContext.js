3f53abb0c1065a51c9f35b77b3aa1353401f016b
Zayd Hassan <zayd_hassan@polysia.com>
2024-11-20 12:38:04 +0530

    Updated ThemeContext.js

--- frontend/src/components/ThemeContext.js
+++ frontend/src/components/ThemeContext.js
@@ -4,6 +4,7 @@ export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('dark');
+  const [showIncidents, setShowIncidents] = useState(true); // default to true
 
   useEffect(() => {
     document.body.className = theme + '-mode';
@@ -12,11 +13,15 @@ export const ThemeProvider = ({ children }) => {
   const toggleTheme = () => {
     setTimeout(() => {
       setTheme(theme === 'dark' ? 'light' : 'dark');
-    }, 400); 
+    }, 400);
+  };
+
+  const toggleIncidents = () => {
+    setShowIncidents(!showIncidents);
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme, showIncidents, toggleIncidents }}>
       {children}
     </ThemeContext.Provider>
   );
