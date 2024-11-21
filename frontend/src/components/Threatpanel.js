cc2307ed198e1c80f4904197ae7d79b13eafb2c8
Zayd Hassan <zayd_hassan@polysia.com>
2024-11-21 16:21:11 +0530

    Updated Threatpanel.js

--- frontend/src/components/Threatpanel.js
+++ frontend/src/components/Threatpanel.js
@@ -1,15 +1,161 @@
+// import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
+// import * as d3 from 'd3';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+// import { debounce } from 'lodash';
+// import { ThemeContext } from './ThemeContext';
+ 
+// const Threatpanel = ({ handleSpeedChange, isSidebarOpen, toggleSidebar, activeAttacks = [] }) => {
+//     const [attackSpeed, setAttackSpeed] = useState(1500);
+//     const [data, setData] = useState([]);
+//     const { theme } = useContext(ThemeContext);
+ 
+//     const debouncedHandleSpeedChange = useMemo(() =>
+//         debounce((newSpeed) => {
+//             handleSpeedChange(newSpeed);
+//         }, 500),
+//     [handleSpeedChange]);
+ 
+//     useEffect(() => {
+//         return () => {
+//             debouncedHandleSpeedChange.cancel();
+//         };
+//     }, [debouncedHandleSpeedChange]);
+ 
+//     const increaseSpeed = useCallback(() => {
+//         setAttackSpeed(prevSpeed => {
+//             const newSpeed = Math.max(500, prevSpeed - 200);
+//             debouncedHandleSpeedChange(newSpeed);
+//             return newSpeed;
+//         });
+//     }, [debouncedHandleSpeedChange]);
+ 
+//     const decreaseSpeed = useCallback(() => {
+//         setAttackSpeed(prevSpeed => {
+//             const newSpeed = prevSpeed + 200;
+//             debouncedHandleSpeedChange(newSpeed);
+//             return newSpeed;
+//         });
+//     }, [debouncedHandleSpeedChange]);
+ 
+//     useEffect(() => {
+//         const socket = new WebSocket('ws://localhost:8000/ws/daily_threats/');
+ 
+//         socket.onopen = () => {
+//             console.log("WebSocket connection established");
+//         };
+ 
+//         socket.onmessage = (event) => {
+//             const message = JSON.parse(event.data);
+ 
+//             if (message.daily_threat_data) {
+//                 const parsedData = message.daily_threat_data.map(d => ({
+//                     date: new Date(d.date),
+//                     value: d.TotalAttacks
+//                 }));
+//                 setData(parsedData);
+//             }
+//         };
+ 
+//         socket.onerror = (error) => {
+//             console.error("WebSocket error:", error);
+//         };
+ 
+//         return () => {
+//             socket.close();
+//         };
+//     }, []);
+ 
+//     useEffect(() => {
+//         if (data.length === 0) return;
+ 
+//         const margin = { top: 20, right: 30, bottom: 30, left: 50 },
+//               width = 230 - margin.left - margin.right,
+//               height = 150 - margin.top - margin.bottom;
+ 
+//         d3.select("#chart svg").remove();
+ 
+//         const svg = d3.select("#chart")
+//           .append("svg")
+//             .attr("width", width + margin.left + margin.right)
+//             .attr("height", height + margin.top + margin.bottom)
+//           .append("g")
+//             .attr("transform", `translate(${margin.left},${margin.top})`);
+ 
+//         const x = d3.scaleTime()
+//           .domain(d3.extent(data, d => d.date))
+//           .range([0, width]);
+ 
+//         const y = d3.scaleLinear()
+//           .domain([0, d3.max(data, d => d.value)])
+//           .range([height, 0]);
+ 
+//         const area = d3.area()
+//           .x(d => x(d.date))
+//           .y0(height)
+//           .y1(d => y(d.value));
+ 
+//         svg.append("path")
+//           .datum(data)
+//           .attr("fill", "#3478f6")
+//           .attr("d", area);
+ 
+//         svg.append("g")
+//           .attr("transform", `translate(0,${height})`)
+//           .call(d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %d")));
+ 
+//         svg.append("g")
+//           .call(d3.axisLeft(y).ticks(5).tickFormat(d3.format(".2s")));
+//     }, [data]);
+ 
+//     return (
+// <div id="leftSidebar" className={theme}>
+// <h2>THREAT DATA</h2>
+// <b>RECENT DAILY ATTACKS</b>
+// <div id="chart"></div>
+// <ul id="threatList">
+// <li>
+// <b>ATTACK SPEED</b>
+// <div className="speed-controls fixed-controls">
+// <button onClick={decreaseSpeed}>
+// <FontAwesomeIcon icon={faMinus} />
+// </button>
+// <span>{attackSpeed} ms</span>
+// <button onClick={increaseSpeed}>
+// <FontAwesomeIcon icon={faPlus} />
+// </button>
+// </div>
+// </li>
+// <li>
+// <b>ACTIVE ATTACKS</b>
+// <ul id="activeAttacksList">
+//  {activeAttacks.map((attack, index) => (
+// <li key={index}>
+//     {attack.sourceName} to {attack.destinationName} ({attack.threatType})
+// </li>
+//                         ))}
+// </ul>
+// </li>
+// </ul>
+// </div>
+//     );
+// };
+ 
+// export default Threatpanel;
+
 import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
 import * as d3 from 'd3';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { debounce } from 'lodash';
 import { ThemeContext } from './ThemeContext';
- 
+
 const Threatpanel = ({ handleSpeedChange, isSidebarOpen, toggleSidebar, activeAttacks = [] }) => {
     const [attackSpeed, setAttackSpeed] = useState(1500);
     const [data, setData] = useState([]);
+    const [isChartEnlarged, setIsChartEnlarged] = useState(false);
     const { theme } = useContext(ThemeContext);
- 
+
     const debouncedHandleSpeedChange = useMemo(() =>
         debounce((newSpeed) => {
             handleSpeedChange(newSpeed);
@@ -37,17 +183,16 @@ const Threatpanel = ({ handleSpeedChange, isSidebarOpen, toggleSidebar, activeAt
             return newSpeed;
         });
     }, [debouncedHandleSpeedChange]);
- 
+
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:8000/ws/daily_threats/');
- 
+
         socket.onopen = () => {
             console.log("WebSocket connection established");
         };
- 
+
         socket.onmessage = (event) => {
             const message = JSON.parse(event.data);
- 
             if (message.daily_threat_data) {
                 const parsedData = message.daily_threat_data.map(d => ({
                     date: new Date(d.date),
@@ -56,89 +201,91 @@ const Threatpanel = ({ handleSpeedChange, isSidebarOpen, toggleSidebar, activeAt
                 setData(parsedData);
             }
         };
- 
+
         socket.onerror = (error) => {
             console.error("WebSocket error:", error);
         };
- 
+
         return () => {
             socket.close();
         };
     }, []);
- 
+
     useEffect(() => {
         if (data.length === 0) return;
- 
+
         const margin = { top: 20, right: 30, bottom: 30, left: 50 },
-              width = 230 - margin.left - margin.right,
-              height = 150 - margin.top - margin.bottom;
- 
+            width = isChartEnlarged ? window.innerWidth * 0.8 - margin.left - margin.right : 230 - margin.left - margin.right,
+            height = isChartEnlarged ? window.innerHeight * 0.6 - margin.top - margin.bottom : 150 - margin.top - margin.bottom;
+
         d3.select("#chart svg").remove();
- 
+
         const svg = d3.select("#chart")
-          .append("svg")
+            .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
-          .append("g")
+            .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
- 
+
         const x = d3.scaleTime()
-          .domain(d3.extent(data, d => d.date))
-          .range([0, width]);
- 
+            .domain(d3.extent(data, d => d.date))
+            .range([0, width]);
+
         const y = d3.scaleLinear()
-          .domain([0, d3.max(data, d => d.value)])
-          .range([height, 0]);
- 
+            .domain([0, d3.max(data, d => d.value)])
+            .range([height, 0]);
+
         const area = d3.area()
-          .x(d => x(d.date))
-          .y0(height)
-          .y1(d => y(d.value));
- 
+            .x(d => x(d.date))
+            .y0(height)
+            .y1(d => y(d.value));
+
         svg.append("path")
-          .datum(data)
-          .attr("fill", "#3478f6")
-          .attr("d", area);
- 
+            .datum(data)
+            .attr("fill", "#3478f6")
+            .attr("d", area);
+
         svg.append("g")
-          .attr("transform", `translate(0,${height})`)
-          .call(d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %d")));
- 
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).ticks(5).tickFormat(d3.timeFormat("%b %d")));
+
         svg.append("g")
-          .call(d3.axisLeft(y).ticks(5).tickFormat(d3.format(".2s")));
-    }, [data]);
- 
+            .call(d3.axisLeft(y).ticks(5).tickFormat(d3.format(".2s")));
+    }, [data, isChartEnlarged]);
+
     return (
-<div id="leftSidebar" className={theme}>
-<h2>THREAT DATA</h2>
-<b>RECENT DAILY ATTACKS</b>
-<div id="chart"></div>
-<ul id="threatList">
-<li>
-<b>ATTACK SPEED</b>
-<div className="speed-controls fixed-controls">
-<button onClick={decreaseSpeed}>
-<FontAwesomeIcon icon={faMinus} />
-</button>
-<span>{attackSpeed} ms</span>
-<button onClick={increaseSpeed}>
-<FontAwesomeIcon icon={faPlus} />
-</button>
-</div>
-</li>
-<li>
-<b>ACTIVE ATTACKS</b>
-<ul id="activeAttacksList">
- {activeAttacks.map((attack, index) => (
-<li key={index}>
-    {attack.sourceName} to {attack.destinationName} ({attack.threatType})
-</li>
+        <div id="leftSidebar" className={theme}>
+            <h2>THREAT DATA</h2>
+            <b>RECENT DAILY ATTACKS</b>
+            <div id="chart" style={{cursor: 'pointer'}} onClick={() => setIsChartEnlarged(!isChartEnlarged)}
+                title={isChartEnlarged ? "Click to Minimize" : "Click to Enlarge"}>
+            </div>
+            <ul id="threatList">
+                <li>
+                    <b>ATTACK SPEED</b>
+                    <div className="speed-controls">
+                        <button onClick={decreaseSpeed}>
+                            <FontAwesomeIcon icon={faMinus} />
+                        </button>
+                        <span>{attackSpeed} ms</span>
+                        <button onClick={increaseSpeed}>
+                            <FontAwesomeIcon icon={faPlus} />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <b>ACTIVE ATTACKS</b>
+                    <ul id="activeAttacksList">
+                        {activeAttacks.map((attack, index) => (
+                            <li key={index}>
+                                {attack.sourceName} to {attack.destinationName} ({attack.threatType})
+                            </li>
                         ))}
-</ul>
-</li>
-</ul>
-</div>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     );
 };
- 
+
 export default Threatpanel;
